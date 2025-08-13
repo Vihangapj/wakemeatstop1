2cd8bbab3cdea9221c0009e79f4329dd864a3de2
Vihangapj <jhmvihanga54@gmail.com>
2025-08-13 10:48:35 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 // Using a hosted URL for the provided logo image.
-const LOGO_URL = "https://github.com/Vihangapj/wakemeatstop1/blob/35f8adbe83343c28f521a317d6cfa44285309acf/logo.png";
+const LOGO_URL = "https://github.com/Vihangapj/wakemeatstop1/blob/main/logo.png";
 
 const Logo: React.FC<{ className?: string }> = ({ className }) => (
     <div className={className}>
